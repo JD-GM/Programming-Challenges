8e1f6397f9cdbe3e0a7530a74f7ff97ca1fc405f
JD <jsdvd.guerrero@gmail.com>
2023-12-05 09:04:57 +0100

    Upload Program

---
+++ 00 - Guess Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Random rand = new Random();
+
+        int random = rand.Next(0,100);
+
+        int number;
+
+        int count = 0;
+
+        do
+        {
+            count++;
+
+            Console.WriteLine("Introduce un número:");
+
+            number = int.Parse(Console.ReadLine());
+
+            if (number < random) Console.WriteLine("Introduce un número mayor.");
+
+            if (number > random) Console.WriteLine("Introduce un número menor.");
+
+        } while (number != random);
+
+        Console.WriteLine($"Correcto. Has consumido {count} intentos.");
+     }
+}
